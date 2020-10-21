80dbe62d572d06a256d6c60916252f057654e581
liu <pliu@mails.jlu.edu.cn>
2020-10-21 21:08:27 +0800

    new course example for gdb

---
+++ gdb/test.c
@@ -0,0 +1,8 @@
+#include<stdio>
+
+int main()
+{
+
+printf("Hello World!");
+
+}
---
+++ gdb/testgdb.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int get_sum(int n)
+{
+int sum=0,i;
+for(i=0;i<n;i++)
+   sum+=i;
+ return sum;
+}
+
+int main()
+{
+int i=100,result;
+result=get_sum(i);
+printf("1+2+...+%d=%d\n",i,result);
+return 0;
+}
